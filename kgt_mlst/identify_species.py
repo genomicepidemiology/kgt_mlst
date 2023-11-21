b3f1c684277923d5799389eb39373dbf8fad30de
MBHallgren <s153002@student.dtu.dk>
2023-11-21 13:50:36 +0100

    1.0.10

--- kgt_mlst/identify_species.py
+++ kgt_mlst/identify_species.py
@@ -7,7 +7,7 @@ def auto_identifiy_species(arguments):
     input_string = " ".join(arguments.input)
     kma.KMARunner(input_string,
                   arguments.output + "/bac_species",
-                  arguments.db_dir + "/bac_species_db/bac_species_db",
+                  arguments.db_dir + "/bac_db/bac_db",
                   "-mem_mode -1t1 -t 8 -Sparse").run()
     with open(arguments.output + '/bac_species.spa', 'r') as f:
         best_score = 0
